43a60e0008fa99ea2ce0e426822d22e3d15ea832
Jakob <jakob.boss@gmail.com>
2014-05-29 18:39:49 +0200

    Better bg quality

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -5,16 +5,16 @@
 */
 
 // Skel.
-	// skel.init({
-	// 	reset: 'full',
-	// 	breakpoints: {
-	// 		'global': { range: '*', href: 'css/style.css', lockViewport: true, viewport: 'minimal-ui' },
-	// 		'wide': { range: '-1680', href: 'css/style-wide.css' },
-	// 		'normal': { range: '-1280', href: 'css/style-normal.css' },
-	// 		'mobile': { range: '-640', href: 'css/style-mobile.css' },
-	// 		'mobilep': { range: '-360', href: 'css/style-mobilep.css' }
-	// 	}
-	// });
+	skel.init({
+		reset: 'full',
+		breakpoints: {
+			'global': { range: '*', href: 'css/style.css', lockViewport: true, viewport: 'minimal-ui' },
+			'wide': { range: '-1680', href: 'css/style-wide.css' },
+			'normal': { range: '-1280', href: 'css/style-normal.css' },
+			'mobile': { range: '-640', href: 'css/style-mobile.css' },
+			'mobilep': { range: '-360', href: 'css/style-mobilep.css' }
+		}
+	});
 
 // Events (JS).
 
